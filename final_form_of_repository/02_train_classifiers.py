ee7b7aad07475e5f6359eebfd7799d6dbe28f3e1
r4 <andras.fekete106@gmail.com>
2024-05-10 15:16:35 +0200

    update training for maximizing auc

--- final_form_of_repository/02_train_classifiers.py
+++ final_form_of_repository/02_train_classifiers.py
@@ -21,7 +21,7 @@ class Models_validator:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-        self.scoring=('accuracy','recall')
+        self.scoring=('accuracy','recall','roc_auc')
     
     def get_scores(self,clf,p1,p2):
         if clf=='rf':
@@ -34,38 +34,42 @@ class Models_validator:
             scores = cross_validate(classifier, pca_component , y, cv=5, scoring=self.scoring)
         return scores
 
-    def train_model(self,clf,range1,range2,min_accuracy=0.7):
+    def maximize_score(self,clf,range1,range2,score_label="roc_auc",min_accuracy=0.0):
         p1_p2=dict()
-        p1_recall=dict()
+        p1_score=dict()
         p1_accuracy=dict()
         for p1 in range1:
             print(p1)
             p2_acc=dict()
-            p2_rec=dict()
+            p2_score=dict()
             for p2 in range2:
                 scores=self.get_scores(clf,p1,p2)
                 accuracy=scores['test_accuracy'].mean()
-                recall=scores['test_recall'].mean()
+                score=scores['test_'+score_label].mean()
+                #roc_auc=scores['test_roc_auc'].mean()
 
                 if accuracy>=min_accuracy:
                     p2_acc[p2]=accuracy
-                    p2_rec[p2]=recall
+                    p2_score[p2]=score
             
             #best_accuracy=max(acc_depth.keys())
-            if len(p2_rec.keys())>0:
-                best_recall=max(p2_rec.values())
-                p1_recall[p1]=best_recall
-                p2_for_best_recall=select_key(p2_rec,best_recall)
-                p1_p2[p1]=p2_for_best_recall
-                p1_accuracy[p1]=p2_acc[p2_for_best_recall]
+            if len(p2_score.keys())>0:
+                best_score=max(p2_score.values())
+                p1_score[p1]=best_score
+                p2_for_best_score=select_key(p2_score,best_score)
+                p1_p2[p1]=p2_for_best_score
+                p1_accuracy[p1]=p2_acc[p2_for_best_score]
         
-        if len(p1_recall.values())>0:
-            maxrec=max(p1_recall.values())
-            p1=select_key(p1_recall,maxrec)
+        if len(p1_score.values())>0:
+            maxscore=max(p1_score.values())
+            p1=select_key(p1_score,maxscore)
             acc=p1_accuracy[p1]
-            print(maxrec)
-            print(acc)
-            return p1,p1_p2[p1]
+            #print(maxscore)
+            #print(acc)
+
+            d = {"score":maxscore,"accuracy":acc,"p1":p1,"p2":p1_p2[p1]}
+            return d
+            #return p1,p1_p2[p1]
         return f"No model with accuracy at least {min_accuracy}"
 
 #Where did we store the features?
@@ -92,13 +96,18 @@ rf_ds_list=list(range(1,16)) # creating list of depths for rf
 best_rf_depth=0  # best depth found after cv
 
 model_validator = Models_validator(X_scaled_test_data,y)
-#rf_trees,rf_depth = model_validator.train_model('rf',range(6,127,5),range(10,11))
+rf_results = model_validator.maximize_score('rf',range(6,127,5),range(1,7)) #roc_auc
 #print(rf_trees,rf_depth)
 #print()
-pca_dimensions,knn_neighbors = model_validator.train_model('knn',range(1,13),range(1,27),min_accuracy=0.67)
+#pca_dimensions,knn_neighbors = model_validator.train_model('knn',range(1,13),range(1,27),min_accuracy=0.67)
+#knn_results = model_validator.maximize_score('knn',range(1,13),range(1,27)) #roc_auc
 
-print(pca_dimensions,knn_neighbors)
-print()
+trees=rf_results["p1"]
+depth=rf_results["p2"]
+rf_roc_auc=rf_results["score"]
+rf_accuracy=rf_results["accuracy"]
+
+print(f"trees: {trees}, depth: {depth}, roc_auc: {rf_roc_auc}, accuracy: {rf_accuracy}")
 exit()
 #Let's say you now decided to use the 5-NN 
 pca = PCA(n_components=pca_dimensions)
